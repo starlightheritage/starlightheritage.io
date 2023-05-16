1a7da5704f4c390d97252ae17a262f66fdf49122
starlightheritage <117940324+starlightheritage@users.noreply.github.com>
2023-05-15 19:23:52 -0600

    Adding extended menu

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+
+const menuToggle = document.querySelector('.menu-toggle');
+const sidebar = document.querySelector('.sidebar');
+
+menuToggle.addEventListener('click', () => {
+  sidebar.classList.toggle('sidebar-expanded');
+});
